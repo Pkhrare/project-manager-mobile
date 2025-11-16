f95cb767e79017096e62cf1038a7609d1349bc66
prakhar <prakhardeep6@gmail.com>
2025-11-16 18:06:26 -0500

    client production 2

--- app.config.js
+++ app.config.js
@@ -4,7 +4,7 @@ export default {
   expo: {
     name: "Waiver On-the-go",
     slug: "project-manager-mobile",
-    version: "1.0.0",
+    version: "1.0.1",
     orientation: "portrait",
     scheme: "projectmanager",
     platforms: ["ios", "android"],
